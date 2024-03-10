4ea996f229ff7c3d02248e8e850c10ee1ca1bbe3
YavaVilar <160225186+YavaVilar@users.noreply.github.com>
2024-03-10 15:03:55 +0100

    Update test-unnest_response.R

--- tests/testthat/test-unnest_response.R
+++ tests/testthat/test-unnest_response.R
@@ -1,4 +1,24 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# 1: Vérification du nombre de lignes pour une prévision de 7 jours
+  test_that("La fonction renvoie le bon nombre de lignes", {
+    expect_equal(nrow(data_test), 168, info = "Le résultat devrait contenir 168 lignes.")
+  })
+
+# 2: Vérification du nom des colonnes en sortie  
+    expected_columns <- c("date_heure", "temperature_celsius", "temperature_ressentie", "precipitation_proba", "precipitation_mm")
+    expect_equal(names(data_test), expected_columns, info = "Les noms des colonnes devraient correspondre à ceux du dataframe.")
+  })
+
+# 3: Vérification des valeurs de la colonne temperature
+test_that("Les valeurs de la colonne temperature correspondent aux valeurs proposées en entrée", {
+  expect_equal(data_test$temperature_celsius, temperatures_celsius, 
+               info = "Les valeurs de la colonne temperature devraient correspondre aux valeurs proposées en entrée.")
 })
+
+# 4: Vérification du nombre de colonnes en sortie
+test_that("Le nombre de colonnes en sortie est correct", {
+  expect_equal(ncol(data_test), length(expected_columns), 
+               info = "Le nombre de colonnes en sortie devrait être égal à celui du vecteur 'expected_columns'.")
+})
+
+
 
